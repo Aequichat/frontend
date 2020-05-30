ad8538582bf6d8f788c0f2f24dccfae86ebf90d9
Juan Diego Lopez <juandlopez@msn.com>
2020-05-30 14:57:49 -0500

    Base structure for login service

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { handleError } = require('./utils');
+
+const usersRoutes = require('./routes/users');
 
 const app = express();
 
-app.get('/api/test', (req, res) => {
-    res.send('Aequichat API REST');
-});
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api', usersRoutes);
+
+app.use(handleError);
+
 module.exports = app;
