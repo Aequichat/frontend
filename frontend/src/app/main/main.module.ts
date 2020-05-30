f3f2f71b959dfa687b1cea2ad1368f6508a2f076
Federico Camacho <federicocamacho97@gmail.com>
2020-05-30 14:05:22 -0500

    Template del chat.

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { StoryCardComponent } from './components/story-card/story-card.component';
-import { LoginComponent } from './pages/login/login.component';
-import { StoriesComponent } from './pages/stories/stories.component';
 import { ChatTemplateComponent } from './pages/chat-template/chat-template.component';
 import { ChatComponent } from './pages/chat/chat.component';
 import { IntroductionComponent } from './pages/introduction/introduction.component';
+import { LoginComponent } from './pages/login/login.component';
+import { StoriesComponent } from './pages/stories/stories.component';
 
 
 @NgModule({
   declarations: [LoginComponent, StoryCardComponent, StoriesComponent, ChatTemplateComponent, ChatComponent, IntroductionComponent],
   imports: [
+    MatSidenavModule,
     CommonModule
   ]
 })
