db4f37b604f412b3a423ec02c5a484e745b4638d
Camilo Gutierrez <camiloguti24@gmail.com>
2020-05-30 15:34:49 -0500

    Login styling and validations

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './pages/login/login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [LoginComponent],
@@ -13,6 +16,9 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class MainModule { }
--- frontend/src/app/main/pages/login/login.component.ts
+++ frontend/src/app/main/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'aequi-login',
@@ -8,12 +8,58 @@ import { FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  public username = new FormControl('');
-  public password = new FormControl('');
+  public userForm: FormGroup;
+  public signUpForm: FormGroup;
+  public isLoading: boolean;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
 
-  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      newUsername: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      repeatPassword: ['', Validators.required],
+      newEmail: ['', [Validators.required, Validators.email]]
+    }, {
+      validators: [this.matchPasswords('newPassword', 'repeatPassword')]
+    }
+    );
   }
+
+  ngOnInit(): void {}
+
+  public login() {
+    console.log(this.userForm.value.username, this.userForm.value.password);
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
+
+  public signUp() {
+    console.log('Signing Up...');
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
+
+  private matchPasswords(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            return;
+        }
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+}
 
 }
