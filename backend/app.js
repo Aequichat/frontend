402e99786d8e40ca1e4fbde35ff48778e9d1ccad
Juan Diego Lopez <juandlopez@msn.com>
2020-05-30 16:58:15 -0500

    Add login and register services

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { handleError } = require('./utils');
-
-const usersRoutes = require('./routes/users');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -11,8 +9,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api', usersRoutes);
-
-app.use(handleError);
+app.use('/api', userRoutes);
 
 module.exports = app;
