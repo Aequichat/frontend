361566258cf01655a55ae8804e26c934ffd17e02
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2020-05-30 15:48:51 -0500

    Dynamic clock added

--- frontend/src/app/main/components/time-transition/time-transition.component.ts
+++ frontend/src/app/main/components/time-transition/time-transition.component.ts
@@ -6,13 +6,22 @@ import {MatDialog, MatDialogRef} from '@angular/material/dialog';
   templateUrl: './time-transition.component.html',
   styleUrls: ['./time-transition.component.scss']
 })
-export class TimeTransitionComponent {
+export class TimeTransitionComponent implements OnChanges {
 
   @Input() message: string;
   @Input() fadeIn: boolean;
+  @Input() hour: number;
 
   ngOnInit(): void {
 
   }
-
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes && changes.hour && changes.hour.currentValue) {
+      this.hour = Math.trunc(Number(changes.hour.currentValue))
+      if (this.hour > 12) {
+        this.hour = this.hour - 12;
+      }
+    }
+  }
 }
