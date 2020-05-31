9ca77ed7bb7e5673d03997537318d9db7e363411
Juan Diego Lopez <juandlopez@msn.com>
2020-05-31 11:58:36 -0500

    Add forgot password service

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,8 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
 const connection = require('../config/db');
+const config = require('../config/env');
 
 /**
  * Login the user
@@ -13,11 +17,11 @@ async function login(req, res) {
             return res.status(400).send({ message: 'Datos incompletos' });
         }
 
-        username = String(username).toLowerCase()
+        username = username.toLowerCase();
 
         const db = await connection.getConnection();
         const usersCollection = db.collection('users');
-        const user = await usersCollection.findOne({ username, password });
+        const user = await usersCollection.findOne({ username, password }, { projection: { password: 0 }});
 
         if (!user) {
             return res.status(404).send({ message: 'Usuario o contraña no validos'});
@@ -29,6 +33,50 @@ async function login(req, res) {
     }
 }
 
+async function restorePassword(req, res) {
+    try {
+        let email = req.body.email;
+
+        if (!email) {
+            return res.status(400).send({ message: 'Por favor digita tu email' });
+        }
+
+        const db = await connection.getConnection();
+        const usersCollection = db.collection('users');
+        const user = await usersCollection.findOne({ email });
+
+        if (!user) {
+            return res.status(404).send({ message: 'El correo ingresado no esta registrado' });
+        }
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: config.emailAuth.address,
+                pass: config.emailAuth.password
+            }
+        });
+
+        let emailTemplate = fs.readFileSync(path.resolve(__dirname, '..', 'html', 'email-template.html')).toString();
+
+        emailTemplate = emailTemplate.replace('${id}', user._id);
+
+        const mailOptions = {
+            from: config.emailAuth.address,
+            to: email,
+            subject: 'Recuperar contraseña',
+            html: emailTemplate
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        return res.status(200).send({ message: 'Te hemos eviado un correo a tu email para recuperar tu contraseña' })
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
 module.exports = {
     login,
+    restorePassword
 };
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const connection = require('../config/db');
+const { ObjectId } = require('mongodb');
 
 /**
  * Register a new user
@@ -16,8 +17,10 @@ async function addUser(req, res) {
         if (!userData.username || !userData.email || !userData.password) {
             return res.status(400).send({ message: 'Datos incompletos' });
         }
-        userData.username = String(userData.username).toLowerCase()
-        userData.email = String(userData.email).toLowerCase()
+
+        userData.username = String(userData.username).toLowerCase();
+        userData.email = String(userData.email).toLowerCase();
+
         const db = await connection.getConnection();
         const usersCollection = db.collection('users');
         const user = await usersCollection.findOne(
@@ -30,12 +33,44 @@ async function addUser(req, res) {
 
         await usersCollection.insertOne(userData);
 
+        delete userData.password;
+
         return res.status(200).send({ message: 'Usuario registrado correctamente!', user: userData });
     } catch (error) {
         return res.status(500).send(error);
     }
 }
 
+async function updateUser(req, res) {
+    try {
+        let _id = req.body.id;
+        let password = req.body.password;
+
+        if (!_id || !password) {
+            return res.status(400).send({ message: 'Datos incompletos' });
+        }
+
+        const db = await connection.getConnection();
+        const usersCollection = db.collection('users');
+
+        const result = await usersCollection.findOneAndUpdate(
+            { _id: new ObjectId(_id) }, { $set: { password } },
+            { returnOriginal: false }
+        );
+
+        if (!result.value) {
+            return res.status(403).send({ message: 'Usuario no existe' });
+        }
+
+        delete result.value.password;
+
+        return res.status(200).send({ message: 'Contraseña actualizada correctamente', user: result.value });
+    } catch (error) {
+        return res.status(500).send({ error: 'ID no valido' });
+    }
+}
+
 module.exports = {
     addUser,
+    updateUser
 };
