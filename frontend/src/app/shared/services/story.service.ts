0b74a62a44debd5100800b78c4ddcb91ecd40a07
Federico Camacho <federicocamacho97@gmail.com>
2020-05-30 19:30:58 -0500

    Historias.

--- frontend/src/app/shared/services/story.service.ts
+++ frontend/src/app/shared/services/story.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Story } from '../models/story.model';
 
 @Injectable({
@@ -8,5 +11,9 @@ export class StoryService {
 
   public stories: Story[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStories(): Observable<Story[]> {
+    return this.http.get<Story[]>(environment.API_URL + 'story');
+  }
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const backendEnpoint = 'http://localhost:3000/api/';
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'http://localhost:3000/api/'
 };
 
 /*
