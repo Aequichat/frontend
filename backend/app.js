d387111ef9f6457d2f9ddb49cb0d64219fe5ee1d
Kevin Arias <kevinariasf@hotmail.com>
2020-05-30 19:53:08 -0500

    Add progress endpoint

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const expressStaticGzip = require('express-static-gzip');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const storyRoutes = require('./routes/story');
+const progressRoutes = require('./routes/progress');
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(bodyParser.json());
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', storyRoutes);
+app.use('/api', progressRoutes);
 
 module.exports = app;
