7e596ae2926b5660fdde2111238045159d06a7bb
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2020-05-30 21:24:42 -0500

    Chat header added

--- frontend/src/app/main/pages/chat/chat.component.ts
+++ frontend/src/app/main/pages/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/shared/models/character.model';
 
 @Component({
   selector: 'aequi-chat',
@@ -6,8 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
+  members: Character[];
+  constructor() {
+    this.members = [
+      {
+        name: 'Prueba',
+        color: '#ff00ff'
+      },
+      {
+        name: 'Camo',
+        color: "#00FFFF"
+      },
+      {
+        name: 'Prueba',
+        color: '#ff00ff'
+      },
+      {
+        name: 'Camo',
+        color: "#00FFFF"
+      },
+      {
+        name: 'Prueba',
+        color: '#ff00ff'
+      },
+      {
+        name: 'Camo',
+        color: "#00FFFF"
+      },
+      {
+        name: 'Prueba',
+        color: '#ff00ff'
+      },
+      {
+        name: 'Camo',
+        color: "#00FFFF"
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
